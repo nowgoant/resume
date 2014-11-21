08b0fafa0427ec32b19d455b7fc5eff1d7956566
majun <631359926@qq.com>
2014-11-21 11:40:58 +0800

    add title

---
+++ assets/js/index.ts
@@ -0,0 +1,309 @@
+﻿/// <reference path="../../scripts/typings/d3/d3.d.ts" />
+/// <reference path="../../scripts/typings/d3/d3.d.ts" />
+/// <reference path="../../scripts/typings/crossfilter/crossfilter.d.ts" />
+/// <reference path="../../scripts/typings/iresumedata.d.ts" />
+/// <reference path="../../scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="timer.ts" />
+
+declare var colorbrewer: any;
+
+'use strict';
+
+function percentage(group: any[], per: number) {
+    var total = 0;
+
+    group.forEach(function (data, index) {
+        total += data.value;
+    });
+
+    return Math.floor(per / total * 100);
+}
+
+function createPercentage(chart: any, group: any, d: any) {
+    if (chart.hasFilter() && !chart.hasFilter(d.key))
+        return "0%";
+    var label = "";
+    if (group.all())
+        label += percentage(group.all(), d.value) + "%";
+    return label;
+}
+
+module Tips {
+    function bindEventHanlder(groupName: string, seletor: string, techTip) {
+        d3.selectAll("#" + groupName + " " + seletor).call(techTip);
+        d3.selectAll("#" + groupName + " " + seletor).on('mouseover', techTip.show).on('mouseout', techTip.hide);
+        return this;
+    }
+
+    export function Tip1(title: string) {
+        return d3.tip().attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function (d) {
+                return "<strong style='color: #f0027f'>" + title + ":</strong> <span>" + d.key + "</span>";
+            });
+    }
+
+    export function CreateTechTip(groupName: string) {
+        bindEventHanlder(groupName, "g.row", this.Tip1("技术名称"));
+        return this;
+    }
+
+    export function CreateBooksTip(groupName: string) {
+        bindEventHanlder(groupName, "g.row", this.Tip1("书名"));
+        return this;
+    }
+
+    export function PieTip() {
+        return d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function (d) { return "<span style='color: #f0027f'>" + d.data.key + "</span> : " + d.value; });
+    }
+
+    export function CreatePieTip(groupName: string) {
+        var pieTip = this.PieTip();
+        d3.selectAll("#" + groupName + " .pie-slice").call(pieTip);
+        d3.selectAll("#" + groupName + " .pie-slice").on('mouseover', pieTip.show).on('mouseout', pieTip.hide);
+        return this;
+    }
+}
+
+(function () {
+    var timeFormat = d3.time.format.iso, projectsHash = {
+        "0": { name: '实习', duration: 1 },
+        "1": { name: '远洋地产SOA', duration: 1.5, year: '201108-201302', company: '明河伟业科技有限公司' },
+        "2": { name: '中石油管道移动平台', duration: 0.5, year: '201304-201310', company: '长城软件' },
+        "3": { name: '微软123导航', duration: 1.1, year: '201310-至今', company: '软通动力' }
+    };
+
+    function filterProjects(d: IResumeDataV2) {
+        return projectsHash[d.project] ? projectsHash[d.project].duration : 0;
+    }
+
+    d3.csv('data/resume-2014.csv', function (resume: any) {
+        var projectNames = [], timelong = [];
+        var GROUPNAME = 'work-area', ndx: any = crossfilter(resume), all = ndx.groupAll(), experChart;
+
+        //技能
+        (function (_ndx) {
+            var techs = _ndx.dimension(function (d) {
+                return d.tech;
+            });
+            //console.log(techs);
+            var resumeByTechGroup = techs.group().reduceSum(function (d) { return +d.duration; });
+
+            var techChart = dc.rowChart("#tech-area", GROUPNAME)
+                .width($("#tech-area").parent().width())
+                .height(810)
+                .dimension(techs)
+                .group(resumeByTechGroup)
+                .elasticX(true)
+                .title(function (d) {
+                    var key = d.key,
+                        value = d.value;
+                    return "技术名称:" + key.toUpperCase() + "\n" +
+                        "使用时间:" + value / 12 + "(年)\n";
+                    return "";
+                });;
+
+        })(ndx);
+
+        //公司
+        (function (_ndx) {
+            var tempData = {};
+            var companys = _ndx.dimension(function (d) {
+                var temp = projectsHash[d.project].company;
+                tempData[temp] = d.project;
+                return temp;
+            });
+
+            var resumeByCompanyGroup = companys.group().reduceSum(function (d) { return +d.duration; });
+
+            var companysChart = dc.pieChart("#company-area", GROUPNAME)
+                .width($("#company-area").parent().width())
+                .height(180)
+                .radius(80)
+                .dimension(companys)
+                .group(resumeByCompanyGroup)
+                .legend(dc.legend().x(10).y(10).itemHeight(20).gap(10))
+                .title(function (d) {
+                    var key = d.key || (d.data && d.data.key),
+                        value = d.value || (d.data && d.data.value);
+                    return "时长:" + projectsHash[tempData[key]]['duration'] + "(年)\n" +
+                        "公司名称:  " + key + "\n";
+                })
+                .label(function (d) {
+                    return createPercentage(companysChart, resumeByCompanyGroup, d);
+                });
+
+        })(ndx);
+
+        //项目经验
+        (function (_ndx) {
+            var projects = _ndx.dimension(function (d) {
+                return projectsHash[d.project].name;
+            });
+
+            var resumeByProjectGroup = projects.group().reduceSum(function (d) { return +d.duration; });
+
+            experChart = dc.pieChart("#projectExperiences-area", GROUPNAME)
+                .width($("#projectExperiences-area").parent().width())
+                .height(250)
+                .radius(125)
+                .innerRadius(50)
+                .dimension(projects)
+                .group(resumeByProjectGroup)
+                .legend(dc.legend().x(10).y(10).itemHeight(20).gap(10))
+                .label(function (d) {
+                    return createPercentage(experChart, resumeByProjectGroup, d);
+                })
+                .title(function (d) {
+                    var key = d.key || (d.data && d.data.key),
+                        value = d.value || (d.data && d.data.value);
+                    return "大小:   " + value + "\n" +
+                        "项目名称:  " + key + "\n";
+                });
+
+        })(ndx);
+
+        //工作时间
+        (function (_ndx) {
+            var tempData = {};
+            var projects = _ndx.dimension(function (d) {
+                tempData[projectsHash[d.project].year] = d.project;
+                return projectsHash[d.project].year;
+            });
+
+            var resumeByProjectGroup = projects.group().reduceSum(function (d) { return +d.duration; });
+
+            var timeChart = dc.pieChart("#year-area", GROUPNAME)
+                .width($("#year-area").parent().width())
+                .height(250)
+                .radius(125)
+                .innerRadius(50)
+                .dimension(projects)
+                .group(resumeByProjectGroup)
+                .legend(dc.legend().x(10).y(10).itemHeight(20).gap(10))
+                .label(function (d) {
+                    return createPercentage(timeChart, resumeByProjectGroup, d);
+                })
+                .title(function (d) {
+                    var key = d.key || (d.data && d.data.key),
+                        value = d.value || (d.data && d.data.value);
+                    return "时长:" + key + "\n" +
+                        "公司名称:  " + projectsHash[tempData[key]]['company'] + "\n";
+                });
+
+        })(ndx);
+
+
+
+
+        dc.renderAll(GROUPNAME);
+        $('.loader', $("#" + GROUPNAME)).remove();
+
+        Tips.CreateTechTip(GROUPNAME).CreatePieTip(GROUPNAME);
+    });
+})();
+
+(function () {
+    $(document).ready(function () {
+        $("#navbar").addClass("slide--up").headroom({
+            tolerance: {
+                down: 10,
+                up: 150
+            },
+            offset: 10,
+            classes: {
+                initial: "slide",
+                pinned: "slide--reset",
+                unpinned: "slide--up"
+            }
+        });
+    });
+})();
+
+
+(function () {
+    var timeFormat = d3.time.format.iso;
+
+    d3.csv('data/interest-2014.csv', function (resume: any) {
+        var GROUPNAME = 'interest-area', ndx: any = crossfilter(resume);
+
+        //兴趣技能类型     
+        (function (_ndx) {
+            var types = _ndx.dimension(function (d) {
+                return d.type;
+            });
+
+            var resumeByTypesGroup = types.group();
+
+            var typsChart = dc.pieChart("#inter-type-area", GROUPNAME)
+                .width($("#inter-type-area").parent().width())
+                .height(250)
+                .radius(125)
+                .innerRadius(50)
+                .dimension(types)
+                .group(resumeByTypesGroup)
+                .legend(dc.legend().x(10).y(10).itemHeight(20).gap(10))
+                .title(function (d) {
+                    var key = d.key || (d.data && d.data.key),
+                        value = d.value || (d.data && d.data.value);
+                    return "类型名称:" + key + "\n";
+                }).label(function (d) {
+                    return createPercentage(typsChart, resumeByTypesGroup, d);
+                });
+
+        })(ndx);
+
+        //兴趣技能
+        (function (_ndx) {
+            var techs = _ndx.dimension(function (d) {
+                return d.tech;
+            });
+            //console.log(techs);
+            var resumeByTechGroup = techs.group().reduceSum(function (d) { return +d.duration; });
+
+            var techChart = dc.rowChart("#inter-tech-area", GROUPNAME)
+                .width($("#inter-tech-area").parent().width())
+                .height(600)
+                .dimension(techs)
+                .group(resumeByTechGroup)
+                .elasticX(true)
+                .title(function (d) {
+                    var key = d.key,
+                        value = d.value;
+                    return "技术名称:" + key.toUpperCase() + "\n" +
+                        "使用时间:" + value / 12 + "(年)\n";
+                });
+
+        })(ndx);
+
+        //书     
+        (function (_ndx) {
+            var books = _ndx.dimension(function (d) {
+                return d.books;
+            });
+
+            var resumeByBooksGroup = books.group().reduceSum(function (d) { return +d.bookCount; });
+
+            var techChart = dc.rowChart("#inter-book-area", GROUPNAME)
+                .width($("#inter-book-area").parent().width())
+                .height(300)
+                .dimension(books)
+                .group(resumeByBooksGroup)
+                .elasticX(true)
+                .title(function (d) {
+                    var key = d.key,
+                        value = d.value;
+                    return "书名:" + key.toUpperCase() + "\n";
+                });
+
+        })(ndx);
+
+        dc.renderAll(GROUPNAME);
+        $('.loader', $("#" + GROUPNAME)).remove();
+
+        Tips.CreateTechTip("inter-tech-area").CreateBooksTip('inter-book-area').CreatePieTip(GROUPNAME);
+    });
+})();
